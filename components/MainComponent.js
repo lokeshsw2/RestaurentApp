e3943c2da92c64872abe314f8bdd9404969317b0
lokeshsw2 <lokeshsw2@gmail.com>
2020-07-15 01:58:04 +0530

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent'
+import  Contact  from "./ContactComponent";
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import Constants from 'expo-constants';
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+);
 
 const MenuNavigator = createStackNavigator(
     {
@@ -50,12 +86,28 @@ const MainNavigator = createDrawerNavigator({
           drawerLabel: 'Home'
         }
     },
+
+    About: { 
+        screen: AboutNavigator,
+        navigationOptions: {
+          title: 'About Us',
+          drawerLabel: 'About Us'
+        }
+    },
+
     Menu: { 
         screen: MenuNavigator,
         navigationOptions: {
           title: 'Menu',
           drawerLabel: 'Menu'
         }, 
+    },
+    Contact: { 
+        screen: ContactNavigator,
+        navigationOptions: {
+          title: 'Contact Us',
+          drawerLabel: 'Contact Us'
+        }
     }
 }, {
   drawerBackgroundColor: '#D1C4E9'
